dcb754d4b1223764a5cedd4156b8d0c66e9da3d7
santyrgh17 <santiagoguereroh034@gmail.com>
2023-07-26 11:53:32 -0500

    --

--- appsillex/lib/main.dart
+++ appsillex/lib/main.dart
@@ -25,48 +25,76 @@ class LoginPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             // Agrega la imagen como fondo utilizando Image.asset
-            Expanded(
-              child: Image.asset(
-                'assets/descarga-removebg-preview.png',
-                // fit: BoxFit.fitWidth,
+            Padding(
+                padding: EdgeInsets.only(left: 20.0, top: 50.0),
+                child: SizedBox(
+                  width: 400, // Ancho deseado
+                  height: 250, // Alto deseado
+                  child: Image.asset('assets/descarga-removebg-preview.png'),
+                )),
+
+            const Padding(
+              padding: EdgeInsets.only(left: 20.0, top: 20.0),
+              child: Text(
+                'Sillex',
+                textAlign: TextAlign
+                    .center, // Aquí puedes cambiar el texto como desees
+                style: TextStyle(
+                  fontSize: 38.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            Text(
-              'Sillex',
-              textAlign:
-                  TextAlign.center, // Aquí puedes cambiar el texto como desees
-              style: TextStyle(
-                fontSize: 38.0,
-                fontWeight: FontWeight.bold,
+
+            const Padding(
+              padding: EdgeInsets.only(left: 20.0, top: 20.0),
+              child: Text(
+                'Sillas elegantes',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
               ),
             ),
-            Text(
-              'Sillas elegantes',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20.0,
+            const Padding(
+              padding: EdgeInsets.only(left: 20.0, top: 20.0),
+              child: Text(
+                '¡Bienvenido!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            Text(
-              '¡Bienvenido!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
+            const SizedBox(height: 16.0),
+
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 20.0,
+                  top: 70.0), // Ajustar los valores para mover el botón
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue[200],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                        20), // Define el radio de las esquinas
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => CreateAccountMenuprincipal()),
+                  );
+                },
+                child: const Text(
+                  'Explorar',
+                  style: TextStyle(fontSize: 30),
+                ),
               ),
             ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => CreateAccountMenuprincipal()),
-                );
-              },
-              child: Text('Explorar'),
-            ),
-            SizedBox(height: 16.0),
+            const SizedBox(height: 16.0),
             TextButton(
               onPressed: () {
                 // Navegar a la segunda página cuando se presione "Crear cuenta"
@@ -76,7 +104,14 @@ class LoginPage extends StatelessWidget {
                       builder: (context) => CreateAccountRegistro()),
                 );
               },
-              child: Text('Registro'),
+              child: const Text(
+                'Registro',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
+              ),
             ),
             TextButton(
               onPressed: () {
@@ -86,7 +121,14 @@ class LoginPage extends StatelessWidget {
                       builder: (context) => CreateAccountIniciarSesion()),
                 );
               },
-              child: Text('Iniciar Sesion'),
+              child: const Text(
+                'Iniciar Sesion',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
+              ),
             ),
           ],
         ),
@@ -99,8 +141,28 @@ class CreateAccountRegistro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Crear cuenta')),
-      body: Padding(
+      appBar: AppBar(
+        title: Text('Registro'),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              // Acción al hacer clic en el botón
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.whi,
+              shape: RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.circular(30), // Define el radio de las esquinas
+              ),
+            ),
+            child: Text('omitir',
+            style: TextStyle(
+              color: Colors.black
+            ),),
+          ),
+        ],
+      ),
+      body: const Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -123,7 +185,7 @@ class CreateAccountMenuprincipal extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Sillex')),
-      body: Padding(
+      body: const Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -146,7 +208,7 @@ class CreateAccountIniciarSesion extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Iniciar Sesion')),
-      body: Padding(
+      body: const Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
