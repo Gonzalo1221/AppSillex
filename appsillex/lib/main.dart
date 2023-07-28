e783edb4fbef6f5b31e3e6d51fcf4825403ab16e
santyrgh17 <santiagoguereroh034@gmail.com>
2023-07-28 12:05:54 -0500

    --

--- appsillex/lib/main.dart
+++ appsillex/lib/main.dart
@@ -1,35 +1,39 @@
 import 'package:flutter/material.dart';
-
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: LoginPage(),
     );
   }
 }
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Color(0xFFEDE7DD), // Set the background color to red
-        padding: EdgeInsets.all(16.0),
+        color: const Color(0xFFEDE7DD), // Set the background color to red
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             // Agrega la imagen como fondo utilizando Image.asset
-            Padding(
+            Container(
+                height: 400,
+                width: 400,
                 padding: EdgeInsets.only(left: 20.0, top: 50.0),
                 child: SizedBox(
                   width: 400, // Ancho deseado
-                  height: 250, // Alto deseado
+                  height: 400, // Alto deseado
                   child: Image.asset('assets/descarga-removebg-preview.png'),
                 )),
 
@@ -90,7 +94,7 @@ class LoginPage extends StatelessWidget {
                 },
                 child: const Text(
                   'Explorar',
-                  style: TextStyle(fontSize: 30),
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                 ),
               ),
             ),
@@ -138,47 +142,225 @@ class LoginPage extends StatelessWidget {
 }
 
 class CreateAccountRegistro extends StatelessWidget {
+  final ScrollController controller = ScrollController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _nombreController = TextEditingController();
+  final TextEditingController _correoController = TextEditingController();
+  final TextEditingController _contrasenaController = TextEditingController();
+  final TextEditingController _confirmarContrasenaController = TextEditingController();
+  final TextEditingController _numeroIdentificacionController = TextEditingController();
+  final TextEditingController _celularController = TextEditingController();
+  
+
+  CreateAccountRegistro({super.key});
+
   @override
+  
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _formKey,
       appBar: AppBar(
-        title: Text('Registro'),
+        title: const Text('Registro'),
         actions: [
           ElevatedButton(
             onPressed: () {
-              // Acción al hacer clic en el botón
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginPage()),
+              ); // Acción al hacer clic en el botón
             },
             style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.whi,
-              shape: RoundedRectangleBorder(
-                borderRadius:
-                    BorderRadius.circular(30), // Define el radio de las esquinas
-              ),
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(
+                      30), // Define el radio de las esquinas
+                ),
+                fixedSize: const Size(10, 40)),
+            child: const Text('omitir',
+              style: TextStyle(color: Colors.black),
             ),
-            child: Text('omitir',
-            style: TextStyle(
-              color: Colors.black
-            ),),
           ),
         ],
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Formulario de creación de cuenta',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text('Nombre',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                TextFormField(
+                  controller: _nombreController,
+                  decoration: InputDecoration(
+                    hintText: 'Ingrese su nombre',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, ingrese su nombre';
+                    }
+                    return null;
+                  },
+                ),
+                const Text('Correo',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                TextFormField(
+                  controller: _correoController,
+                  decoration: const InputDecoration(
+                    hintText: 'Ingrese su correo',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, ingrese su correo';
+                    }
+                    return null;
+                  },
+                ),
+                const Text(
+                  'Contraseña',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                TextFormField(
+                  controller: _contrasenaController,
+                  decoration: const InputDecoration(
+                    hintText: 'Ingrese su Contraseña',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, ingrese su Contraseña';
+                    }
+                    return null;
+                  },
+                ),
+                const Text(
+                  'Confirmar Contraseña',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                TextFormField(
+                  controller: _confirmarContrasenaController,
+                  decoration: const InputDecoration(
+                    hintText: 'Ingrese su Contraseña nueva',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, ingrese su Contraseña nueva';
+                    }
+                    if (value != _contrasenaController.text) {
+                      return 'Las contraseñas no coinciden';
+                    }
+                    return null;
+                  },
+                ),
+                const Text(
+                  'Genero',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+
+ 
+
+                const Text(
+                  'Ciudad',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter your email',
+                  ),
+                ),
+                const Text(
+                  'Fecha de nacimiento',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter your email',
+                  ),
+                ),
+                const Text('Tipo de identificacion',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const TextField(decoration: InputDecoration(
+                    hintText: 'Enter your email',
+                  ),
+                ),
+                const Text(
+                  'Numero de identificacion',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                TextFormField(
+                  controller: _numeroIdentificacionController,
+                  decoration: const InputDecoration(
+                    hintText: 'Ingrese su identificacion',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, ingrese su identificacion';
+                    }
+                    return null;
+                  },
+                ),
+                const Text('Celular',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                TextFormField(
+                  controller: _celularController,
+                  decoration: const InputDecoration(
+                    hintText: 'Ingrese su numero',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, ingrese su numero de celular';
+                    }
+                    return null;
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+
+                      // Si el formulario es válido, realizar acciones adicionales aquí
+                      // Por ejemplo, guardar los datos en una base de datos o enviarlos al servidor
+                    }
+                  },
+                  child: const Text('Finalizar'),
+                ),
+                // Aquí puedes agregar campos de texto y widgets adicionales para el formulario de creación de cuenta
+              ],
             ),
-            // Aquí puedes agregar campos de texto y widgets adicionales para el formulario de creación de cuenta
-          ],
+          ),
         ),
       ),
     );
   }
 }
+// void _alerta(BuildContext context) {
+//   showDialog(
+//       barrierDismissible: false,
+//       context: context,
+//       child: AlertDialog(
+//         title: Text("hola"),
+//         context: Text("¿que haces?"),
+//         actions: [
+//          TextButton(
+//             child: Text("No"),
+//             onPressed: () {
+//               print("no");
+//             }),
+//           TextButton(
+//             child: Text("Siuuu"),
+//             onPressed: () {
+//               print("Si");
+//             },
+//             )
+//            ],
+//            ));
+// }
 
 class CreateAccountMenuprincipal extends StatelessWidget {
   @override
@@ -220,6 +402,102 @@ class CreateAccountIniciarSesion extends StatelessWidget {
             ),
             // Aquí puedes agregar campos de texto y widgets adicionales para el formulario de creación de cuenta
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class LExpansionPanelList extends StatefulWidget {
+  @override
+  _LExpansionPanelListState createState() => _LExpansionPanelListState();
+}
+
+class _LExpansionPanelListState extends State<LExpansionPanelList> {
+  int index = -1;
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          child: ExpansionPanelList(
+            expansionCallback: (i, isOpen) {
+              setState(() {
+                if (index == i)
+                  index = -1;
+                else
+                  index = i;
+              });
+            },
+            animationDuration: Duration(seconds: 1),
+            dividerColor: Colors.teal,
+            elevation: 2,
+            children: [
+              ExpansionPanel(
+                headerBuilder: (BuildContext context, bool isExpanded) {
+                  return ListTile(
+                    title: Text("Index 0 (Header Tap Enabled)"),
+                  );
+                },
+                canTapOnHeader: true,
+                body: ListTile(
+                  title: Text("Notes"),
+                  subtitle: const Text('for index 0'),
+                ),
+                isExpanded: index == 0,
+              ),
+              ExpansionPanel(
+                headerBuilder: (BuildContext context, bool isExpanded) {
+                  return ListTile(
+                    title: Text("Index 1"),
+                  );
+                },
+                body: ListTile(
+                  title: Text("Notes"),
+                  subtitle: const Text('for index 1'),
+                ),
+                isExpanded: index == 1,
+              ),
+              ExpansionPanel(
+                headerBuilder: (BuildContext context, bool isExpanded) {
+                  return ListTile(
+                    title: Text("Index 2 (Header Tap Enabled)"),
+                  );
+                },
+                canTapOnHeader: true,
+                body: ListTile(
+                  title: Text("Notes"),
+                  subtitle: const Text('for index 2'),
+                ),
+                isExpanded: index == 2,
+              ),
+              ExpansionPanel(
+                headerBuilder: (BuildContext context, bool isExpanded) {
+                  return ListTile(
+                    title: Text("Index 3"),
+                  );
+                },
+                body: ListTile(
+                  title: Text("Notes"),
+                  subtitle: const Text('for index 3'),
+                ),
+                isExpanded: index == 3,
+              ),
+              ExpansionPanel(
+                headerBuilder: (BuildContext context, bool isExpanded) {
+                  return ListTile(
+                    title: Text("Index 4"),
+                  );
+                },
+                body: ListTile(
+                  title: Text("Notes"),
+                  subtitle: const Text('for index 4'),
+                ),
+                isExpanded: index == 4,
+              ),
+            ],
+          ),
         ),
       ),
     );
