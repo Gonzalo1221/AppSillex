9a3d90ccb219025b9eafd903edb167ce1d8b8a1c
santyrgh17 <santiagoguereroh034@gmail.com>
2023-07-31 11:46:20 -0500

    cambio

--- appsillex/lib/main.dart
+++ appsillex/lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -28,14 +32,13 @@ class LoginPage extends StatelessWidget {
           children: [
             // Agrega la imagen como fondo utilizando Image.asset
             Container(
-                height: 400,
-                width: 400,
-                padding: EdgeInsets.only(left: 20.0, top: 50.0),
-                child: SizedBox(
-                  width: 400, // Ancho deseado
-                  height: 400, // Alto deseado
-                  child: Image.asset('assets/descarga-removebg-preview.png'),
-                )),
+              height: 400,
+              width: 400,
+              child: Image.asset(
+                'assets/descarga-removebg-preview.png',
+                fit: BoxFit.cover,
+              ),
+            ),
 
             const Padding(
               padding: EdgeInsets.only(left: 20.0, top: 20.0),
@@ -44,7 +47,7 @@ class LoginPage extends StatelessWidget {
                 textAlign: TextAlign
                     .center, // Aquí puedes cambiar el texto como desees
                 style: TextStyle(
-                  fontSize: 38.0,
+                  fontSize: 50.0,
                   fontWeight: FontWeight.bold,
                 ),
               ),
@@ -56,7 +59,7 @@ class LoginPage extends StatelessWidget {
                 'Sillas elegantes',
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  fontSize: 20.0,
+                  fontSize: 28.0,
                 ),
               ),
             ),
@@ -66,7 +69,7 @@ class LoginPage extends StatelessWidget {
                 '¡Bienvenido!',
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  fontSize: 28.0,
+                  fontSize: 34.0,
                   fontWeight: FontWeight.bold,
                 ),
               ),
@@ -75,15 +78,17 @@ class LoginPage extends StatelessWidget {
 
             Padding(
               padding: const EdgeInsets.only(
-                  left: 20.0,
+                  left: 10.0,
                   top: 70.0), // Ajustar los valores para mover el botón
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   primary: Colors.blue[200],
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(
-                        20), // Define el radio de las esquinas
+                        40), // Define el radio de las esquinas
                   ),
+                  minimumSize:
+                      const Size(250, 60), // Ajusta el tamaño mínimo del botón
                 ),
                 onPressed: () {
                   Navigator.push(
@@ -99,41 +104,87 @@ class LoginPage extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 16.0),
-            TextButton(
-              onPressed: () {
-                // Navegar a la segunda página cuando se presione "Crear cuenta"
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => CreateAccountRegistro()),
-                );
-              },
-              child: const Text(
-                'Registro',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18.0,
+            Row(
+              mainAxisAlignment:
+                  MainAxisAlignment.center, // Alineación central horizontal
+              children: [
+                TextButton(
+                  onPressed: () {
+                    // Navegar a la segunda página cuando se presione "Registro"
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CreateAccountRegistro()),
+                    );
+                  },
+                  child: const Text(
+                    'Registro',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24.0,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => CreateAccountIniciarSesion()),
-                );
-              },
-              child: const Text(
-                'Iniciar Sesion',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18.0,
+                const SizedBox(width: 20), // Un pequeño espacio entre los botones
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CreateAccountIniciarSesion()),
+                    );
+                  },
+                  child: const Text(
+                    'Iniciar Sesión',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24.0,
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
+            const SizedBox(width: 50), // Un pequeño espacio entre los botones
+            TermsAndConditionsWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TermsAndConditionsWidget extends StatelessWidget {
+  final String termsText = 'Términos y condiciones';
+  final String termsUrl ='https://www.example.com'; // Reemplaza con la URL real de tus términos y condiciones
+
+  _launchURL() async {
+    if (await canLaunch(termsUrl)) {
+      await launch(termsUrl);
+    } else {
+      throw 'No se pudo abrir la URL $termsUrl';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      // Agregamos el Center para centrar el texto
+      child: RichText(
+        text: TextSpan(
+          text: 'Acepto los ',
+          style: const TextStyle(color: Colors.black),
+          children: <TextSpan>[
+            TextSpan(
+              text: termsText,
+              style: const TextStyle(
+                color: Colors.blue,
+                decoration: TextDecoration.underline,
+              ),
+              recognizer: TapGestureRecognizer()..onTap = _launchURL,
+            ),
+            const TextSpan(text: '.'),
           ],
         ),
       ),
@@ -148,15 +199,15 @@ class CreateAccountRegistro extends StatelessWidget {
   final TextEditingController _nombreController = TextEditingController();
   final TextEditingController _correoController = TextEditingController();
   final TextEditingController _contrasenaController = TextEditingController();
-  final TextEditingController _confirmarContrasenaController = TextEditingController();
-  final TextEditingController _numeroIdentificacionController = TextEditingController();
+  final TextEditingController _confirmarContrasenaController =
+      TextEditingController();
+  final TextEditingController _numeroIdentificacionController =
+      TextEditingController();
   final TextEditingController _celularController = TextEditingController();
-  
 
   CreateAccountRegistro({super.key});
 
   @override
-  
   Widget build(BuildContext context) {
     return Scaffold(
       key: _formKey,
@@ -177,7 +228,8 @@ class CreateAccountRegistro extends StatelessWidget {
                       30), // Define el radio de las esquinas
                 ),
                 fixedSize: const Size(10, 40)),
-            child: const Text('omitir',
+            child: const Text(
+              'omitir',
               style: TextStyle(color: Colors.black),
             ),
           ),
@@ -192,7 +244,8 @@ class CreateAccountRegistro extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                const Text('Nombre',
+                const Text(
+                  'Nombre',
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
                 TextFormField(
@@ -207,7 +260,8 @@ class CreateAccountRegistro extends StatelessWidget {
                     return null;
                   },
                 ),
-                const Text('Correo',
+                const Text(
+                  'Correo',
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
                 TextFormField(
@@ -262,8 +316,6 @@ class CreateAccountRegistro extends StatelessWidget {
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
 
- 
-
                 const Text(
                   'Ciudad',
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
@@ -282,10 +334,12 @@ class CreateAccountRegistro extends StatelessWidget {
                     hintText: 'Enter your email',
                   ),
                 ),
-                const Text('Tipo de identificacion',
+                const Text(
+                  'Tipo de identificacion',
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
-                const TextField(decoration: InputDecoration(
+                const TextField(
+                  decoration: InputDecoration(
                     hintText: 'Enter your email',
                   ),
                 ),
@@ -305,7 +359,8 @@ class CreateAccountRegistro extends StatelessWidget {
                     return null;
                   },
                 ),
-                const Text('Celular',
+                const Text(
+                  'Celular',
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
                 TextFormField(
@@ -323,9 +378,7 @@ class CreateAccountRegistro extends StatelessWidget {
                 ElevatedButton(
                   onPressed: () {
                     if (_formKey.currentState!.validate()) {
-
-                      // Si el formulario es válido, realizar acciones adicionales aquí
-                      // Por ejemplo, guardar los datos en una base de datos o enviarlos al servidor
+                      _showAlertDialog(context);
                     }
                   },
                   child: const Text('Finalizar'),
@@ -338,29 +391,35 @@ class CreateAccountRegistro extends StatelessWidget {
       ),
     );
   }
-}
-// void _alerta(BuildContext context) {
+
+//   void _mostraralert(BuildContext context) {
 //   showDialog(
 //       barrierDismissible: false,
 //       context: context,
 //       child: AlertDialog(
-//         title: Text("hola"),
-//         context: Text("¿que haces?"),
-//         actions: [
-//          TextButton(
-//             child: Text("No"),
-//             onPressed: () {
-//               print("no");
-//             }),
-//           TextButton(
-//             child: Text("Siuuu"),
-//             onPressed: () {
-//               print("Si");
-//             },
-//             )
-//            ],
-//            ));
+//         title: Text("Hola"),
+//         content: Text("¿Que haces?"),
+//       ));
 // }
+  void _showAlertDialog(BuildContext context) {
+    showCupertinoModalPopup<void>(
+      context: context,
+      builder: (BuildContext context) => CupertinoAlertDialog(
+        title: const Text('Mensaje'),
+        content: const Text('¡Registro Exitoso!'),
+        actions: <CupertinoDialogAction>[
+          CupertinoDialogAction(
+            isDefaultAction: true,
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('ok'),
+          )
+        ],
+      ),
+    );
+  }
+}
 
 class CreateAccountMenuprincipal extends StatelessWidget {
   @override
@@ -402,102 +461,6 @@ class CreateAccountIniciarSesion extends StatelessWidget {
             ),
             // Aquí puedes agregar campos de texto y widgets adicionales para el formulario de creación de cuenta
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class LExpansionPanelList extends StatefulWidget {
-  @override
-  _LExpansionPanelListState createState() => _LExpansionPanelListState();
-}
-
-class _LExpansionPanelListState extends State<LExpansionPanelList> {
-  int index = -1;
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          child: ExpansionPanelList(
-            expansionCallback: (i, isOpen) {
-              setState(() {
-                if (index == i)
-                  index = -1;
-                else
-                  index = i;
-              });
-            },
-            animationDuration: Duration(seconds: 1),
-            dividerColor: Colors.teal,
-            elevation: 2,
-            children: [
-              ExpansionPanel(
-                headerBuilder: (BuildContext context, bool isExpanded) {
-                  return ListTile(
-                    title: Text("Index 0 (Header Tap Enabled)"),
-                  );
-                },
-                canTapOnHeader: true,
-                body: ListTile(
-                  title: Text("Notes"),
-                  subtitle: const Text('for index 0'),
-                ),
-                isExpanded: index == 0,
-              ),
-              ExpansionPanel(
-                headerBuilder: (BuildContext context, bool isExpanded) {
-                  return ListTile(
-                    title: Text("Index 1"),
-                  );
-                },
-                body: ListTile(
-                  title: Text("Notes"),
-                  subtitle: const Text('for index 1'),
-                ),
-                isExpanded: index == 1,
-              ),
-              ExpansionPanel(
-                headerBuilder: (BuildContext context, bool isExpanded) {
-                  return ListTile(
-                    title: Text("Index 2 (Header Tap Enabled)"),
-                  );
-                },
-                canTapOnHeader: true,
-                body: ListTile(
-                  title: Text("Notes"),
-                  subtitle: const Text('for index 2'),
-                ),
-                isExpanded: index == 2,
-              ),
-              ExpansionPanel(
-                headerBuilder: (BuildContext context, bool isExpanded) {
-                  return ListTile(
-                    title: Text("Index 3"),
-                  );
-                },
-                body: ListTile(
-                  title: Text("Notes"),
-                  subtitle: const Text('for index 3'),
-                ),
-                isExpanded: index == 3,
-              ),
-              ExpansionPanel(
-                headerBuilder: (BuildContext context, bool isExpanded) {
-                  return ListTile(
-                    title: Text("Index 4"),
-                  );
-                },
-                body: ListTile(
-                  title: Text("Notes"),
-                  subtitle: const Text('for index 4'),
-                ),
-                isExpanded: index == 4,
-              ),
-            ],
-          ),
         ),
       ),
     );
